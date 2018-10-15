03bd51b2410c6a53ee51c92092af1fbcdf2cc909
Vivek <vivekstorm4@gmail.com>
2018-10-15 22:36:17 +0530

    updated code

---
+++ matrixchain.py
@@ -0,0 +1,21 @@
+import sys
+import argparse
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--list","-l",help="Enter the list seperated by space",required=True)
+    args=parser.parse_args()
+    list=args.list
+    a=[int(x) for x in list.split(',')]
+    a=[1]+a+[1]
+    prize = [[0]*len(a) for _ in range(len(a))]
+    n=len(a)
+    print('----------------------')
+    for L in range(2, n):
+        for i in range(1, n-L+1):
+            j = i+L-1
+            prize[i][j] = -sys.maxsize
+            for k in range(i, j):
+                prize[i][j]=max(prize[i][j],prize[i][k] + prize[k+1][j] + a[i-1]*a[k]*a[j])
+
+    print(prize[1][n-1])
